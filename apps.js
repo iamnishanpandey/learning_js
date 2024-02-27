3d09068b7d46f680c50f33ed970fc1b1cbcb4bec
iamnishanpandey <nishanp993@gmail.com>
2024-02-27 17:30:55 +0545

    entry added

--- apps.js
+++ apps.js
@@ -17,14 +17,46 @@ const bur = document.querySelector("#bur");
 const god = document.querySelector(".god");
 
 god.addEventListener('click',function(){
-    bur.style.display='flex';
+    bur.style.display='block';
+    let ahr= prompt("enter h for heads and t for tails");
+    
+    
+    
+    
     let i = Math.random();
     if (i<0.5){
-        answer="it's heads, congratulation!"
+        answer= "h";
+        console.log("heads");
     }
     else{
-        answer="it's tails, congratulation!"
+        answer = "t";
+        console.log("tails");
     }
-    bur.innerHTML = `${answer}`;
-})
 
+    if ( ahr == answer ){
+        if (answer == "t"){
+            bur.innerHTML = `it's tails, congratulations!`;
+        }
+        else{
+            bur.innerHTML = `it's heads, congratulations!`;
+        }
+    }
+    else {
+        if (i>0.5){
+            bur.innerHTML = `it's tails, You Lost!`;
+        }
+        else if (i<0.5){
+            bur.innerHTML = `it's heads, You Lost!`;
+        }
+        else {
+            bur.innerHTML = `Invalid Input, input h or l `;
+        }
+    }
+    
+    god.style.display= 'none';
+})
+bur.addEventListener('click',function(){
+    god.style.display='block';
+    bur.style.display='none';
+}
+)
